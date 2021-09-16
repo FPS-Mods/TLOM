2f89ac724c1ccc40c37877e2a2d695366e60b472
FinchyMcFinch <alex@2am.com.au>
2021-09-16 18:51:56 +1000

    Add Helmet Effects

--- src/main/java/com/fps/tlom/events/ModClientEvents.java
+++ src/main/java/com/fps/tlom/events/ModClientEvents.java
@@ -2,9 +2,12 @@ package com.fps.tlom.events;
 
 import com.fps.tlom.TLOM;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,6 +21,15 @@ public class ModClientEvents {
         LivingEntity player = event.getEntityLiving();
         if (player.getMainHandItem().getItem() == Items.STICK){
             TLOM.LOGGER.info("Player tried to jump with a stick!");
+            player.addEffect(new EffectInstance(Effects.LEVITATION, 100));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onArmorTick(final World world, final PlayerEntity player, final ItemStack itemstack) {
+
+        if (player.getArmorSlots(itemstack = ItemStack.getItem() == Items.DIAMOND_HELMET)){
+            TLOM.LOGGER.info("Player wearing diamond helmet!");
             player.addEffect(new EffectInstance(Effects.LEVITATION, 100));
         }
     }
